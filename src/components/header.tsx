b6882e7bd615d846cd8ac8dfda057031c9ff8a40
Parth099 <parthnpatel715@gmail.com>
2022-03-04 13:41:12 -0500

    "Dumb" nav bar has styled for now

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ interface navProp {
 }
 
 function NavBar(props: navProp) {
-    return <h1 className="title-font">{props.name}</h1>;
+    return <h1 className="title-font bg-wblack text-white text-center text-3xl py-2 border-b-half border-wgray">{props.name}</h1>;
 }
 
 export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                wblack: "#121213",
+                wgray: "#3a3a3c",
+            },
+            borderWidth: {
+                half: ".5px",
+            },
+        },
+    },
+    plugins: [],
+};
